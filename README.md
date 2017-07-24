5f924a8e3c753f6055cd06ab9938e1f7058a76ca
ShubhamPunjabi24 <shubhampnjb@gmail.com>
2017-07-24 19:59:09 +0530

    Update and rename README.md to abc.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# App1
-Yo junta, presenting my first and the most ambitious project by me ever, stay tuned!
---
+++ abc.py
@@ -0,0 +1,2 @@
+# App1
+#Yo junta, presenting my first and the most ambitious project by me ever, stay tuned!
